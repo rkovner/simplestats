242e61b73b4a57e648a6fceda4e7a43ed76ac3ef
RothemKovner <kovnerrothem@gmail.com>
2015-08-27 13:30:04 -0500

    added more tests to test_stats.py

--- test_stats.py
+++ test_stats.py
@@ -7,3 +7,11 @@ test_mean()
 def test_float_mean():
     assert(mean([1,2]) ==1.5)
 test_float_mean()
+
+def test_negative_mean:
+    assert(mean([-4, -2]) == -3)
+test_negative_mean()
+
+#def test_float_mean:
+#    assert(mean([2.5, 4.5, 6.0]) == 4.33333)
+#test_negative_mean()
